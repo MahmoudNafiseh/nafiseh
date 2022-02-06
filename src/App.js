f442d35508aeef5bd113bbb1167eb653dcb3c372
Mahmoud Nafiseh <pokemoncoraige@gmail.com>
2022-02-06 04:05:33 +0200

    almost done with landing page, added animated logo, animated the greeting text, made it responsive.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,166 @@
 import './App.css';
 import anime from 'animejs/lib/anime.es.js';
 import { useState, useEffect, useRef } from 'react';
+import Logo from './Logo';
 function App() {
    const creativeRef = useRef(null);
+   const beginText = useRef(null);
+   const gradText = useRef(null);
+   const [beginTextWidth, setBeginTextWidth] = useState(0);
+   const [beginTextHeight, setBeginTextHeight] = useState(0);
+   const [gradTextWidth, setGradTextWidth] = useState(0);
+   const [gradTextHeight, setGradTextHeight] = useState(0);
    useEffect(() => {
+      setBeginTextWidth(beginText.current.offsetWidth);
+      setBeginTextHeight(beginText.current.offsetHeight);
+      setGradTextWidth(gradText.current.offsetWidth);
+      setGradTextHeight(gradText.current.offsetHeight);
+   }, []);
+   useEffect(() => {
+      beginText.current = anime
+         .timeline({ loop: false })
+         .add({
+            targets: '.begin-text-overlay',
+            width: beginTextWidth + 10,
+            height:
+               window.innerHeight < 1000
+                  ? beginTextHeight + 10
+                  : beginTextHeight + 25,
+            duration: 500,
+            delay: 2000,
+            easing: 'easeInQuad',
+            direction: 'normal',
+         })
+         .add({
+            targets: '.begin-text-overlay',
+            height: 0,
+            translateY: window.innerHeight < 1000 ? 20 : 55,
+            duration: 500,
+            direction: 'normal',
+            easing: 'easeInQuad',
+         });
+      beginText.current = anime.timeline({ loop: false }).add({
+         targets: '.begin-text',
+         opacity: [0, 1],
+         duration: 100,
+         delay: 2500,
+         easing: 'easeInOutQuad',
+      });
+
+      creativeRef.current = anime
+         .timeline({ loop: false })
+         .add({
+            targets: '.creative-text-overlay',
+            height: gradTextHeight,
+            width: [
+               beginTextWidth + 10,
+               beginTextWidth + 10,
+               gradTextWidth,
+               gradTextWidth,
+            ],
+            duration: 1000,
+            delay: 3000,
+            direction: 'normal',
+            easing: 'easeInQuad',
+         })
+         .add({
+            targets: '.creative-text-overlay',
+            width: 0,
+            height: gradTextHeight,
+            duration: 500,
+            easing: 'easeOutQuad',
+            direction: 'normal',
+         });
+
+      creativeRef.current = anime.timeline({ loop: false }).add({
+         targets: '.texts-container',
+         opacity: 1,
+         duration: 100,
+         delay: 4000,
+         easing: 'easeInOutQuad',
+      });
+
       creativeRef.current = anime
          .timeline({ loop: true })
          .add({
             targets: '.hero2 .text1',
-            opacity: [0, 1],
-            duration: 1000,
+            opacity: 1,
+            delay: 3000,
+            duration: 2000,
          })
          .add({
             targets: '.hero2 .text1',
             opacity: 0,
-            duration: 800,
+            duration: 1200,
             easing: 'easeInExpo',
             delay: 500,
          })
          .add({
             targets: '.hero2 .text2',
             opacity: [0, 1],
-            duration: 1000,
+            duration: 2000,
          })
          .add({
             targets: '.hero2 .text2',
             opacity: 0,
-            duration: 800,
+            duration: 1200,
             easing: 'easeInExpo',
             delay: 500,
          })
          .add({
             targets: '.hero2 .text3',
             opacity: [0, 1],
-            duration: 1000,
+            duration: 2000,
          })
          .add({
             targets: '.hero2 .text3',
             opacity: 0,
-            duration: 800,
+            duration: 1200,
             easing: 'easeInExpo',
-            delay: 2000,
+            delay: 1000,
          });
-   }, []);
+   }, [beginTextWidth, gradTextHeight]);
    return (
       <div className='App'>
+         <div className='nav'>
+            <div className='logo-container'>
+               <Logo />
+            </div>
+         </div>
+
          <section className='hero'>
             <div className='header'>
-               <h1>Meet your next</h1>
+               <h1
+                  className='begin-text-overlay'
+                  style={{
+                     height:
+                        window.innerHeight < 1000
+                           ? beginTextHeight + 10
+                           : beginTextHeight + 25,
+                     backgroundColor: 'white',
+                     position: 'absolute',
+                  }}
+               ></h1>
+               <h1
+                  className='begin-text'
+                  ref={beginText}
+                  style={{ opacity: 0 }}
+               >
+                  Meet your next
+               </h1>
+
                <h1 className='hero2'>
-                  <div className='texts-container'>
-                     <span className='creative-text text1'>
+                  <div className='texts-container texts-container-overlay'>
+                     <span
+                        style={{ height: 0, width: beginTextWidth + 10 }}
+                        className='creative-text-overlay text1'
+                     >
+                        Front-End Developer
+                     </span>
+                  </div>
+
+                  <div style={{ opacity: 0 }} className='texts-container'>
+                     <span ref={gradText} className='creative-text text1'>
                         Front-End Developer
                      </span>{' '}
                      <span className='creative-text text2'>
